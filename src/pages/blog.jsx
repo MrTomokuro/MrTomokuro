446a6fd55d4a94fca4bb089a55e36a43e78a894f
Tomoya Kuroda <tomoya@impactnorth.com>
2021-12-30 15:34:22 -0500

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,34 +1,49 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
         data.allHashNodePost.edges.map(edge => (
-          <li key={edge.node.title}>
-            {edge.node.title}
-          </li>
+          <article key={edge.node.slug}>
+            <h2> {edge.node.title}</h2>
+            <p>Posted: {edge.node.dateAdded}</p>
+            {/* <MDXRenderer> */}
+              {edge.node.contentMarkdown}
+            {/* </MDXRenderer> */}
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
 
 
 export const query = graphql`
-  query {
-    allHashNodePost {
-      edges {
-        node {
-          title
+query  {
+  allHashNodePost(sort: {fields: dateAdded, order: DESC}) {
+    edges {
+      node {
+        title
+        coverImage {
+          # File node (to be used with gatsby-plugin-image)
+          childImageSharp {
+            gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED)
+          }
+          # OR
+          # url // "https://cdn.hashnode.com/...
         }
+        slug
+        dateAdded
+        contentMarkdown
+        brief
       }
     }
   }
+}
 `
 
 export default BlogPage
